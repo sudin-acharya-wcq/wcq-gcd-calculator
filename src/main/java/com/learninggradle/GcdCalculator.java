7a046718159b678fe840d8d387a505c62fed2b20
Sudin Acharya <sudin.acharya@workcoverqld.com.au>
2024-08-28 10:29:25 +1000

    first commit

---
+++ src/main/java/com/learninggradle/GcdCalculator.java
@@ -0,0 +1,31 @@
+package com.learninggradle;
+
+
+//function to find gcd of two numbers
+public class GcdCalculator {
+    static int gcd(int a, int b){
+        if(a==0)
+            return b;
+        return gcd(b % a, a);
+    }
+
+//    function to find gcd of n numbers
+    static int gcdArray(int array[]){
+        int result = array[0];
+        for(int element: array){
+            result = gcd(result, element);
+            if(result == 1){
+                return 1;
+            }
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args){
+
+        int[] input = {20,30,50,70};
+        System.out.println(gcdArray(input));
+    }
+}
---
+++ src/test/java/com/learninggradle/GcdTest.java
@@ -0,0 +1,19 @@
+package com.learninggradle;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GcdTest {
+
+    @Test
+    public void testGcd(){
+
+        assertEquals(4,GcdCalculator.gcdArray(new int[] {4})); //Testing with 1 integer
+        assertEquals(1,GcdCalculator.gcdArray(new int[] {2,3})); //Testing with set of 2 integers
+        assertEquals(5,GcdCalculator.gcdArray(new int[] {5,10,15})); //Testing with set of 3 integers
+        assertEquals(1,GcdCalculator.gcdArray(new int[] {2,3,5,7})); //Testing with set of 4 integers
+
+    }
+
+}
